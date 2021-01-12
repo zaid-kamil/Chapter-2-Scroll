69b7927fedaaba231b9a02a1a52342f2e860f113
Zaid kamil <xaidmetamorphos@gmail.com>
2021-01-12 12:16:52 +0530

    Initial commit

---
+++ app/src/main/java/com/digi/chapter2scroll/MainActivity.java
@@ -0,0 +1,14 @@
+package com.digi.chapter2scroll;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
